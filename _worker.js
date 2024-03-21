674e5faca537468347bb3a56cf85434ebf92d0af
rzlly <renzhiliang@gmail.com>
2024-03-21 15:07:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -17,6 +17,7 @@ async function getContentFromUrl(url) {
 
 export default {
   async fetch(request) {
+    getContentFromUrl(speedtesturl);
     let targetUrl = "https://download.parallels.com/desktop/v17/17.1.1-51537/ParallelsDesktop-17.1.1-51537.dmg"; //getContentFromUrl(speedtesturl);
     let cfRequest = new Request(targetUrl, request);
     let response = await fetch(cfRequest);
